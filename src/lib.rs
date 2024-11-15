aedc665f771bdf98b6120bd38eeb75dee88c2ea9
kda <mail@kda.name>
2024-11-14 19:54:55 -0600

    feat: resize image

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 use image::imageops::crop_imm;
-use image::{DynamicImage, GenericImage, RgbaImage};
+use image::{imageops::FilterType, DynamicImage, GenericImage, GenericImageView, RgbaImage};
 use std::error::Error;
 use walkdir::WalkDir;
 
-/// Supported file extensions for image files.
 const SUPPORTED_EXTENSIONS: [&str; 2] = ["png", "webp"];
 
 /// A struct that generates sprite sheets by arranging images in a grid layout
@@ -12,6 +11,8 @@ pub struct Spriterator {
     dir_path: String,
     max_width: u32,
     max_height: u32,
+    image_width: Option<u32>,
+    image_height: Option<u32>,
 }
 
 impl Spriterator {
@@ -22,16 +23,26 @@ impl Spriterator {
     /// * `dir_path` - Path to the directory containing images.
     /// * `max_width` - Maximum width for each sprite.
     /// * `max_height` - Maximum height for each sprite.
-    pub fn new(dir_path: &str, max_width: u32, max_height: u32) -> Self {
+    /// * `image_width` - Optional width for resizing images.
+    /// * `image_height` - Optional height for resizing images.
+    pub fn new(
+        dir_path: &str,
+        max_width: u32,
+        max_height: u32,
+        image_width: Option<u32>,
+        image_height: Option<u32>,
+    ) -> Self {
         Self {
             dir_path: dir_path.to_string(),
             max_width,
             max_height,
+            image_width,
+            image_height,
         }
     }
 
-    /// Generates a vector of sprites by arranging images in rows, respecting the specified maximum
-    /// width and height. Each sprite is trimmed to remove transparent areas.
+    /// Generates a vector of sprites by arranging images in rows.
+    /// Each sprite is trimmed to remove transparent areas.
     ///
     /// # Returns
     ///
@@ -74,13 +85,6 @@ impl Spriterator {
         Ok(sprites)
     }
 
-    /// Retrieves images from the specified directory that match the supported file extensions
-    /// and checks their dimensions.
-    ///
-    /// # Returns
-    ///
-    /// A `Result` containing a vector of `DynamicImage` objects on success, or an error if
-    /// no valid images are found or loading fails.
     fn get_images(&self) -> Result<Vec<DynamicImage>, Box<dyn Error>> {
         let images: Vec<DynamicImage> = WalkDir::new(&self.dir_path)
             .into_iter()
@@ -96,14 +100,14 @@ impl Spriterator {
                 if path.is_file() && is_image {
                     let img = image::open(&path)
                         .map_err(|e| {
-                            eprintln!("Error opening image {}: {}", path.display(), e);
+                            eprintln!("Failed to open image {}: {}. The image has been skipped.", path.display(), e);
                             e
                         })
                         .ok()?;
 
-                    if img.width() > self.max_width || img.height() > self.max_height {
+                        if (self.image_width.is_none() && img.width() > self.max_width) || (self.image_height.is_none() && img.height() > self.max_height) {
                         eprintln!(
-                            "Error: Image {} dimensions {}x{} exceed max dimensions {}x{}.",
+                            "Image {} dimensions {}x{} exceed max dimensions {}x{}. The image has been skipped.",
                             path.display(),
                             img.width(),
                             img.height(),
@@ -111,9 +115,9 @@ impl Spriterator {
                             self.max_height
                         );
                         return None;
+                    } else {
+                        Some(self.resize_image(img))
                     }
-
-                    Some(img)
                 } else {
                     None
                 }
@@ -131,19 +135,11 @@ impl Spriterator {
         Ok(images)
     }
 
-    /// Trims transparent areas from the sprite by cropping to the smallest non-transparent area.
-    ///
-    /// # Arguments
-    ///
-    /// * `sprite` - The sprite image to trim.
-    ///
-    /// # Returns
-    ///
-    /// An `RgbaImage` containing the trimmed sprite.
     fn trim_transparent(&self, sprite: &RgbaImage) -> RgbaImage {
         let (mut max_x, mut max_y) = (0, 0);
         let mut min_x = sprite.width();
         let mut min_y = sprite.height();
+        let mut is_completely_transparent = true;
 
         for (x, y, pixel) in sprite.enumerate_pixels() {
             if pixel[3] > 0 {
@@ -151,15 +147,32 @@ impl Spriterator {
                 max_y = max_y.max(y);
                 min_x = min_x.min(x);
                 min_y = min_y.min(y);
+                is_completely_transparent = false;
             }
         }
 
-        let is_completely_transparent = max_x == 0 && max_y == 0 && sprite.get_pixel(0, 0)[3] == 0;
         if is_completely_transparent {
             return RgbaImage::new(1, 1);
         }
 
         crop_imm(sprite, min_x, min_y, max_x - min_x + 1, max_y - min_y + 1).to_image()
+    }
+
+    fn resize_image(&self, img: DynamicImage) -> DynamicImage {
+        let (original_width, original_height) = img.dimensions();
+
+        match (self.image_width, self.image_height) {
+            (Some(width), Some(height)) => img.resize_exact(width, height, FilterType::Lanczos3),
+            (Some(width), None) => {
+                let height = (original_height * width) / original_width;
+                img.resize_exact(width, height, FilterType::Lanczos3)
+            }
+            (None, Some(height)) => {
+                let width = (original_width * height) / original_height;
+                img.resize_exact(width, height, FilterType::Lanczos3)
+            }
+            (None, None) => img,
+        }
     }
 }
 
@@ -170,7 +183,7 @@ mod tests {
 
     #[test]
     fn test_spriterator_creation() {
-        let spriterator = Spriterator::new("test_dir", 1024, 1024);
+        let spriterator = Spriterator::new("test_dir", 1024, 1024, None, None);
         assert_eq!(spriterator.dir_path, "test_dir");
         assert_eq!(spriterator.max_width, 1024);
         assert_eq!(spriterator.max_height, 1024);
@@ -178,14 +191,14 @@ mod tests {
 
     #[test]
     fn test_empty_directory_error() {
-        let spriterator = Spriterator::new("empty_dir", 1024, 1024);
+        let spriterator = Spriterator::new("empty_dir", 1024, 1024, None, None);
         let result = spriterator.generate();
         assert!(result.is_err());
     }
 
     #[test]
     fn test_trim_transparent() {
-        let spriterator = Spriterator::new("test_dir", 1024, 1024);
+        let spriterator = Spriterator::new("test_dir", 1024, 1024, None, None);
         let mut image = RgbaImage::new(10, 10);
         for x in 2..8 {
             for y in 2..8 {
@@ -195,5 +208,32 @@ mod tests {
         let trimmed = spriterator.trim_transparent(&image);
         assert_eq!(trimmed.width(), 6);
         assert_eq!(trimmed.height(), 6);
+    }
+
+    #[test]
+    fn test_resize_image1() {
+        let spriterator = Spriterator::new("test_dir", 100, 100, Some(10), Some(10));
+        let image = image::DynamicImage::ImageRgba8(RgbaImage::new(10, 10));
+        let resized = spriterator.resize_image(image);
+        assert_eq!(resized.width(), 10);
+        assert_eq!(resized.height(), 10);
+    }
+
+    #[test]
+    fn test_resize_image2() {
+        let spriterator = Spriterator::new("test_dir", 100, 100, Some(10), None);
+        let image = image::DynamicImage::ImageRgba8(RgbaImage::new(20, 20));
+        let resized = spriterator.resize_image(image);
+        assert_eq!(resized.width(), 10);
+        assert_eq!(resized.height(), (20 * 10) / 20);
+    }
+
+    #[test]
+    fn test_resize_image3() {
+        let spriterator = Spriterator::new("test_dir", 100, 100, None, Some(10));
+        let image = image::DynamicImage::ImageRgba8(RgbaImage::new(30, 30));
+        let resized = spriterator.resize_image(image);
+        assert_eq!(resized.width(), (30 * 10) / 30);
+        assert_eq!(resized.height(), 10);
     }
 }

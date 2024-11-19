27a3e27843b05039d62bad29c9969d28adc73b21
kda <mail@kda.name>
2024-11-18 21:57:34 -0600

    feat: frames test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -18,6 +18,18 @@ fn spriterator_test() -> Result<(), Box<dyn Error>> {
     let input_dir = env::var("INPUT_DIR").expect("INPUT_DIR must be set");
     let output_dir = env::var("OUTPUT_DIR").expect("OUTPUT_DIR must be set");
     let ext = env::var("FILE_EXTENSION").expect("FILE_EXTENSION must be set");
+    let large_image_size: u32 = env::var("LARGE_IMAGE_SIZE")
+        .expect("LARGE_IMAGE_SIZE must be set")
+        .parse()
+        .unwrap();
+    let medium_image_size: u32 = env::var("MEDIUM_IMAGE_SIZE")
+        .expect("MEDIUM_IMAGE_SIZE must be set")
+        .parse()
+        .unwrap();
+    let small_image_size: u32 = env::var("SMALL_IMAGE_SIZE")
+        .expect("SMALL_IMAGE_SIZE must be set")
+        .parse()
+        .unwrap();
 
     let output_dir = format!("{}/{}", output_dir, ext);
 
@@ -31,8 +43,8 @@ fn spriterator_test() -> Result<(), Box<dyn Error>> {
         format!("{}/{}", input_dir, ext).as_str(),
         2048,
         2048,
-        Some(64),
-        Some(64),
+        None,
+        None,
     );
 
     let sprites = spriterator.generate();
@@ -48,7 +60,37 @@ fn spriterator_test() -> Result<(), Box<dyn Error>> {
         let frames = sprite.get_frames();
         assert!(!frames.is_empty(), "Sprite {} has no frames", index);
 
-        let sprite_path = format!("{}/{}.{}", output_dir, index, ext);
+        let expected_frames = vec![
+            (0, 0, medium_image_size, medium_image_size),
+            (medium_image_size, 0, small_image_size, small_image_size),
+            (
+                medium_image_size + small_image_size,
+                0,
+                large_image_size,
+                large_image_size,
+            ),
+        ];
+
+        for (i, frame) in frames.iter().enumerate() {
+            let (expected_x, expected_y, expected_width, expected_height) =
+                expected_frames.get(i).expect("Unexpected frame count");
+            assert_eq!(frame.get_x(), *expected_x, "Frame {} X mismatch", i);
+            assert_eq!(frame.get_y(), *expected_y, "Frame {} Y mismatch", i);
+            assert_eq!(
+                frame.get_width(),
+                *expected_width,
+                "Frame {} width mismatch",
+                i
+            );
+            assert_eq!(
+                frame.get_height(),
+                *expected_height,
+                "Frame {} height mismatch",
+                i
+            );
+        }
+
+        let sprite_path = format!("{}/{}.{}", output_dir, index + 1, ext);
         sprite.save(&sprite_path)?;
 
         assert!(
